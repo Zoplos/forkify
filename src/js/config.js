4848636d574b415efd72c77353fb96c0714f26d9
Zoplos <giannis97z97z@gmail.com>
2021-12-11 17:34:58 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a5e7b10e-a8c5-4409-81e8-39e32e1f0e73';
+export const MODAL_CLOSE_SEC = 2.5;
